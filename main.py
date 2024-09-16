ba4e1e8c7b66631b69c8d51e450bc0254d6e2c55
Sammy <bavesh2006kumar@gmail.com>
2024-09-16 17:19:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=(25,35,45,55,65,75,85,95)
+y=("apple","bannana","orange","mango","grapes","pineapple","kiwi","watermelon")
+plt.pie(x,labels=y)
+plt.show()
+# pie chart code can do bar graph and other also
